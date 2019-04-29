5d0baac55958240827edcfab67d2d9e640fea50d
Arjun Attam <arjunattam@gmail.com>
2019-04-29 13:37:18 -0700

    Update names

--- src/main/java/org/springframework/samples/petclinic/items/Collar.java
+++ src/main/java/org/springframework/samples/petclinic/items/Collar.java
@@ -3,7 +3,7 @@ package org.springframework.samples.petclinic.items;
 import org.springframework.samples.petclinic.store.IStoreItem;
 
 public class Collar implements IStoreItem {
-    private String name = "Unused dog collar";
+    private String collarName = "Unused dog collar";
 
     @Override
     public double getPrice() {
