95637735cbdd601dfa5227ed75ed7bb8236faa48
Arjun Attam <arjunattam@gmail.com>
2019-04-29 13:02:20 -0700

    Add store item interface

---
+++ src/main/java/org/springframework/samples/petclinic/items/Collar.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic.items;
+
+import org.springframework.samples.petclinic.store.IStoreItem;
+
+public class Collar implements IStoreItem {
+    private String name = "Unused dog collar";
+
+    @Override
+    public double getPrice() {
+        return 100.0;
+    }
+
+    @Override
+    public double getDiscount() {
+        return 5.0;
+    }
+
+}
