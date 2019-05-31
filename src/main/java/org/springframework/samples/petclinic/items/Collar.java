e19357629fd5e696aee6adf6e69f383aa33690cf
Arjun Attam <arjunattam@gmail.com>
2019-05-31 13:32:48 -0700

    Update Collar.java

--- src/main/java/org/springframework/samples/petclinic/items/Collar.java
+++ src/main/java/org/springframework/samples/petclinic/items/Collar.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.items;
 
 import org.springframework.samples.petclinic.store.IStoreItem;
 
+
 public class Collar implements IStoreItem {
     private String collarName = "Unused dog collar";
 
